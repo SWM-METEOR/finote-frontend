91bcfbeced01a2fda33269eacfae10341fa22824
권유리 <99jiyusu@gmail.com>
2023-07-24 00:35:18 +0900

    [FIN-62] fix: 서버 사이드 렌더링 시 에디터에서 오류 나는 문제 해결 - toasu-ui/editor import를 동적 import 방식으로 변경

--- app/components/Editor.tsx
+++ app/components/Editor.tsx
@@ -1,28 +1,40 @@
 'use client';
-import { useRef, useEffect } from 'react';
-import Editor from '@toast-ui/editor';
+import { useRef, useEffect, useState } from 'react';
 
 export default function EditorComponent() {
   const editElement = useRef(null);
-  const editInstance = useRef<Editor | null>(null);
+  const [editor, setEditor] = useState<any>(null);
 
   useEffect(() => {
-    if (editElement.current == null) {
-      return;
-    }
+    /**
+     * 동적 import
+     * 서버측 렌더링이 끝나고
+     * 클라이언트 측에서 컴포넌트가 렌더링될 때 @toast-ui/editor를 동적으로 불러옴
+     */
+    if (typeof window !== 'undefined') {
+      import('@toast-ui/editor').then((editorModule) => {
+        const Editor = editorModule.Editor;
 
-    editInstance.current = new Editor({
-      el: editElement.current,
-      height: '700px',
-      initialEditType: 'markdown',
-      previewStyle: 'vertical',
-      hooks: {
-        addImageBlobHook(blob, callback) {
-          // console.log(blob);
-        },
-      },
-    });
-  });
+        if (!editElement.current) {
+          return;
+        }
+
+        const newEditor = new Editor({
+          el: editElement.current,
+          height: '700px',
+          initialEditType: 'markdown',
+          previewStyle: 'vertical',
+          hooks: {
+            addImageBlobHook(blob, callback) {
+              // console.log(blob);
+            },
+          },
+        });
+
+        setEditor(newEditor);
+      });
+    }
+  }, []);
 
   return <div ref={editElement}></div>;
 }
