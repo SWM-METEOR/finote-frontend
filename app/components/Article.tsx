3b74f4c0d27e222d5bb4a83d948904f634c045d7
권유리 <99jiyusu@gmail.com>
2023-07-27 20:47:07 +0900

    [FIN-120] feat: AI 검색 관련 전역 상태 추가

--- app/components/Article.tsx
+++ app/components/Article.tsx
@@ -1,7 +1,11 @@
 'use client';
 import { useState, useRef } from 'react';
-import { useTooltipStore } from '@/store/sidePanel';
-import { useSidePanelStore } from '@/store/sidePanel';
+import {
+  useTooltipStore,
+  useSidePanelStore,
+  useSelectedTextStore,
+  useAISearchStore,
+} from '@/store/sidePanel';
 import { SIDEPANEL_OPTION_LIST } from '@/constants/sidePanel';
 
 interface PropsType {
@@ -12,13 +16,14 @@ interface PropsType {
 export default function Article({ contents }: PropsType) {
   const { setSelectedMode } = useTooltipStore();
   const { setIsOpenSidePanel } = useSidePanelStore();
-
-  const [selectedText, setSelectedText] = useState('');
+  const { setSelectedText } = useSelectedTextStore();
+  const { setIsLoadingAISearchResult } = useAISearchStore();
   const [showTooltip, setShowTooltip] = useState(false);
 
   const dragStartX = useRef<number>(0);
   const dragStartY = useRef<number>(0);
   const toolTip = useRef<HTMLDivElement>(null);
+  const tempSelectedTextRef = useRef<string>('');
 
   const handleDragStart = (e: React.MouseEvent<Element, MouseEvent>) => {
     dragStartX.current = e.clientX;
@@ -33,8 +38,9 @@ export default function Article({ contents }: PropsType) {
       return;
     }
 
-    // 드래그된 문장
-    setSelectedText(window.getSelection()!.toString());
+    // 드래그된 텍스트 저장
+    tempSelectedTextRef.current = window.getSelection()!.toString();
+    console.log('tempSelectedText', tempSelectedTextRef.current);
 
     const eventTarget = e.currentTarget as HTMLDivElement;
     const rect = eventTarget.getBoundingClientRect();
@@ -47,12 +53,17 @@ export default function Article({ contents }: PropsType) {
     setShowTooltip(true);
   };
 
+  // 툴팁에서 모드 선택
   const handleClick = (selectedText: string) => {
     setSelectedMode(selectedText); // 선택된 모드 변경
     setIsOpenSidePanel(true); // 사이드 패널 열기
     setShowTooltip(false); // 툴팁 닫기
 
+    setSelectedText(tempSelectedTextRef.current); // 사이드패널에 드래그 텍스트 업데이트
     // TODO: 로딩 스피너 띄우고, 결과 받아오기
+    if (selectedText === SIDEPANEL_OPTION_LIST[0] && tempSelectedTextRef.current !== '') {
+      setIsLoadingAISearchResult(true);
+    }
   };
 
   return (
--- store/sidePanel.ts
+++ store/sidePanel.ts
@@ -19,3 +19,23 @@ export const useTooltipStore = create<TooltipTypes>((set) => ({
   selectedMode: 'default',
   setSelectedMode: (value: string) => set((state) => ({ selectedMode: value })),
 }));
+
+interface SelectedTextTypes {
+  selectedText: string;
+  setSelectedText: (value: string) => void;
+}
+
+export const useSelectedTextStore = create<SelectedTextTypes>((set) => ({
+  selectedText: '',
+  setSelectedText: (value: string) => set((state) => ({ selectedText: value })),
+}));
+
+interface AISearchTypes {
+  isLoadingAISearchResult: boolean;
+  setIsLoadingAISearchResult: (value: boolean) => void;
+}
+// isWaitingForSearchResult: true,
+export const useAISearchStore = create<AISearchTypes>((set) => ({
+  isLoadingAISearchResult: false,
+  setIsLoadingAISearchResult: (value: boolean) => set(() => ({ isLoadingAISearchResult: value })),
+}));
