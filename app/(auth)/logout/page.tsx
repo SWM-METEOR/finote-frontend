7a2da788ca5b052f07356756e4c02d7a4d1db5ef
권유리 <99jiyusu@gmail.com>
2023-08-11 19:19:46 +0900

    [FIN-178] feat: 로그아웃 API 연동

--- app/(auth)/logout/page.tsx
+++ app/(auth)/logout/page.tsx
@@ -1,3 +1,32 @@
+'use client';
+
+import { getCookie, deleteCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation';
+
 export default function LogoutPage() {
-  return <div>로그아웃 페이지</div>;
+  const router = useRouter();
+  const accessToken = getCookie('accessToken');
+  const refreshToken = getCookie('refreshToken');
+
+  // 로그아웃 API 호출
+  fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/logout`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({ accessToken: accessToken, refreshToken: refreshToken }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      deleteCookie('accessToken');
+      deleteCookie('refreshToken');
+      router.push('/');
+    })
+    .catch((error) => {
+      console.error('로그아웃 실패:', error);
+    });
+
+  return <div></div>;
 }
