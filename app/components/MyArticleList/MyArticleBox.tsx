ccb2236fcf2de846f7d26498a2e348ca6bdab778
권유리 <99jiyusu@gmail.com>
2023-08-24 20:15:42 +0900

    [FIN-186] feat: 내 블로그 홈 임시 data 연결

--- app/components/MyArticleList/MyArticleBox.tsx
+++ app/components/MyArticleList/MyArticleBox.tsx
@@ -5,35 +5,45 @@ import GreaterThanIcon from '@/app/components/Icons/GreaterThanIcon';
 import HeartIcon from '@/app/components/Icons/HeartIcon';
 import CommentIcon from '@/app/components/Icons/CommentIcon';
 
-export default function MyArticleBox() {
+interface PropsType {
+  id: number;
+  title: string;
+  body: string;
+  totalLike: number;
+  reply: number;
+  authorNickname: string;
+  date: string;
+  thumbnail: string;
+}
+
+export default function MyArticleBox({
+  title,
+  body,
+  totalLike,
+  reply,
+  date,
+  thumbnail,
+}: PropsType) {
   return (
     <div className="w-[488px] h-[178px] bg-white rounded-[20px] p-[20px] border border-[#EEEEEE] shadow-[0_0_10px_0_rgba(0,0,0,0.05)]">
       <div className="flex gap-[20px]">
-        <div className="w-[140px] h-[140px] rounded-[20px] overflow-hidden flex-shrink-0">
-          <Image
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2qL4TnNIt-zmP-lncsFEHdAbP3Hwn1z29vQ&usqp=CAU"
-            alt="logo"
-            width="140"
-            height="140"
-          />
+        <div className="relative w-[140px] h-[140px] rounded-[20px] overflow-hidden flex-shrink-0">
+          <Image fill className="object-cover" src={thumbnail} alt="logo" sizes="100%" />
         </div>
         <div className="flex flex-col gap-[10px]">
           <p className="flex text-[13px] gap-1 items-center">
-            <span>프론트엔드</span>
+            <span>CS</span>
             <GreaterThanIcon />
-            <span className="text-[#666666] font-bold">자바스크립트</span>
+            <span className="text-[#666666] font-bold">네트워크</span>
           </p>
-          <p className="text-[15px] font-bold">next.js로 웹 서비스 만들기</p>
-          <div className="text-[#666666] text-[12px] line-clamp-2">
-            자바스크립트는 기본적으로 싱글스레드이기 때문에, CPU 코어의 개수와 상관없이 메인
-            스레드에서만 작업을 실행한다. 반면, 자바는 멀티스레드
-          </div>
+          <p className="text-[15px] font-bold line-clamp-1">{title}</p>
+          <div className="text-[#666666] text-[12px] line-clamp-2">{body}</div>
           <p className="flex items-center mt-[8px] text-[13px]">
             <HeartIcon />
-            <span className={`text-[#666666] ml-[2px] mr-[10px]`}>3</span>
+            <span className={`text-[#666666] ml-[2px] mr-[10px]`}>{totalLike}</span>
             <CommentIcon />
-            <span className={`text-[#666666] ml-[2px]`}>10</span>
-            <span className={`text-[#999999] text-[12px] ml-auto`}>2023.08.01</span>
+            <span className={`text-[#666666] ml-[2px]`}>{reply}</span>
+            <span className={`text-[#999999] text-[12px] ml-auto`}>{date}</span>
           </p>
         </div>
       </div>
--- app/components/MyArticleList/MyArticleListContainer.tsx
+++ app/components/MyArticleList/MyArticleListContainer.tsx
@@ -1,7 +1,20 @@
 import MyArticleListView from '@/app/components/MyArticleList/MyArticleListView';
+// import axiosInstance from '@/utils/axios';
+import { articleData } from '@/constants/mockdata';
+// async function getUsersArticles() {
+//   try {
+//     const res = await axiosInstance.get(`/users/articles/all?page=${1}`);
+//     return res.data;
+//   } catch (error) {
+//     throw new Error('Failed to fetch article data');
+//   }
+// }
 
-export default function MyArticleListContainer() {
+export default async function MyArticleListContainer() {
   // 데이터 패칭
+  // const articleRes = await getUsersArticles();
+  // const articleData = articleRes.data;
+  // console.log(articleData);
 
-  return <MyArticleListView />;
+  return <MyArticleListView articleList={articleData.articleList} />;
 }
