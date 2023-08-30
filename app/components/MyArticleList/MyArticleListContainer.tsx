10f5ebaeed76b9a29f99123a547512a892b0b59e
권유리 <99jiyusu@gmail.com>
2023-08-30 23:54:44 +0900

    [FIN-188] feat: 내 글 모아보기 인피니티 스크롤 적용

--- app/components/MyArticleList/MyArticleListContainer.tsx
+++ app/components/MyArticleList/MyArticleListContainer.tsx
@@ -25,23 +25,52 @@ interface ArticleDataType {
 
 export default function MyArticleListContainer({ nickname }: PropsType) {
   const [articleData, setArticleData] = useState<ArticleDataType | null>(null);
+  const [page, setPage] = useState(1);
+  const [hasMoreItems, setHasMoreItems] = useState(true);
+  const maxReqSize = 10;
 
-  useEffect(() => {
-    async function getUsersArticles(nickname: string) {
-      try {
-        const res = await axiosInstance.get(`/articles/${nickname}/all?page=${1}`);
-        const articleRes = res.data.data;
-        setArticleData(articleRes);
-      } catch (error) {
-        throw new Error('Failed to fetch article data');
-      }
-    }
-
+  async function loadMoreUsersArticles(nickname: string) {
+    if (!hasMoreItems) return;
     if (!nickname) return;
-    getUsersArticles(nickname);
-  }, [nickname]);
+
+    try {
+      const res = await axiosInstance.get(
+        `/articles/${nickname}/all?page=${page}&size=${maxReqSize}`
+      );
+      const articleRes = res.data.data;
+
+      if (articleRes.articleList.length === 0) {
+        setHasMoreItems(false);
+        return;
+      }
+
+      if (articleData) {
+        setArticleData((prevData) => ({
+          ...articleRes,
+          articleList: [...(prevData?.articleList || []), ...articleRes.articleList],
+        }));
+      } else {
+        setArticleData(articleRes);
+      }
+    } catch (error) {
+      throw new Error('Failed to fetch article data');
+    }
+  }
+
+  // initial fetch
+  useEffect(() => {
+    loadMoreUsersArticles(nickname);
+  }, []);
 
   if (!articleData) return null;
 
-  return <MyArticleListView articleList={articleData.articleList} />;
+  // nickname값 전달을 위해 익명함수 안에서 호출(클로저)
+  return (
+    <MyArticleListView
+      articleList={articleData.articleList}
+      loadMoreItems={() => loadMoreUsersArticles(nickname)}
+      page={page}
+      setPage={setPage}
+    />
+  );
 }
--- app/components/MyArticleList/MyArticleListView.tsx
+++ app/components/MyArticleList/MyArticleListView.tsx
@@ -1,4 +1,7 @@
+import React, { useState, useEffect, useRef } from 'react';
+
 import MyArticleBox from '@/app/components/MyArticleList/MyArticleBox';
+// import ClipLoader from 'react-spinners/ClipLoader';
 
 interface PropsType {
   articleList: {
@@ -11,9 +14,53 @@ interface PropsType {
     date: string;
     thumbnail: string;
   }[];
+  loadMoreItems: () => Promise<void>;
+  page: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
-export default function MyArticleListView({ articleList }: PropsType) {
+export default function MyArticleListView({
+  articleList,
+  loadMoreItems,
+  page,
+  setPage,
+}: PropsType) {
+  const [loading, setLoading] = useState(false);
+  const observerRef = useRef(null);
+
+  useEffect(() => {
+    if (page === 1) return;
+
+    const loadItems = async () => {
+      setLoading(true);
+      await loadMoreItems();
+      setLoading(false);
+    };
+
+    loadItems();
+  }, [page]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      },
+      { threshold: 0.1 }
+    );
+
+    if (observerRef.current) {
+      observer.observe(observerRef.current);
+    }
+
+    return () => {
+      if (observerRef.current) {
+        observer.unobserve(observerRef.current);
+      }
+    };
+  }, []);
+
   return (
     <div className="w-[1000px] mt-[40px]">
       <p className="text-[20px] font-bold">전체보기</p>
@@ -23,6 +70,13 @@ export default function MyArticleListView({ articleList }: PropsType) {
           <MyArticleBox key={article.id} {...article} />
         ))}
       </div>
+      <div ref={observerRef}></div>
+      {/* TODO: 로딩 시 스켈레톤 UI 띄울지 여부 확정 */}
+      {/* {loading && (
+        <div className="h-20 flex justify-center items-center">
+          <ClipLoader color="#00A1FF" />
+        </div>
+      )} */}
     </div>
   );
 }
