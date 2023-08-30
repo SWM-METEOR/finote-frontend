188386e61dd162884d994caeecc0ddfc540e0fe3
권유리 <99jiyusu@gmail.com>
2023-08-31 00:54:16 +0900

    [FIN-269] feat: 헤더에 내 블로그 이름 렌더링

--- app/components/Header/HeaderContainer.tsx
+++ app/components/Header/HeaderContainer.tsx
@@ -1,7 +1,35 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import { getCookie } from 'cookies-next';
+
+import axiosInstance from '@/utils/axios';
 import HeaderView from '@/app/components/Header/HeaderView';
 
 export default function HeaderContainer() {
-  return <HeaderView />;
+  const pathname = usePathname();
+  const [blogName, setBlogName] = useState('');
+  const [accessToken, setAccessToken] = useState<string>('');
+
+  async function getUserBlogInfo() {
+    if (!accessToken) return;
+
+    try {
+      const res = await axiosInstance.get('/users/blog-info');
+      setBlogName(res.data.data.blogName);
+    } catch (error) {
+      throw new Error('Failed to fetch user blog info');
+    }
+  }
+
+  useEffect(() => {
+    setAccessToken(getCookie('accessToken') as string);
+  }, [pathname]);
+
+  useEffect(() => {
+    getUserBlogInfo();
+  }, [accessToken]);
+
+  return <HeaderView blogName={blogName} accessToken={accessToken} pathname={pathname} />;
 }
