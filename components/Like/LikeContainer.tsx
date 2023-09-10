407b34d246ace38d5aa8d0bfd88815c02c558f2a
권유리 <99jiyusu@gmail.com>
2023-09-11 08:52:13 +0900

    [FIN-266] feat: 비로그인 유저 좋아요 방지

--- components/Like/LikeContainer.tsx
+++ components/Like/LikeContainer.tsx
@@ -1,5 +1,9 @@
 'use client';
 import { useState, useEffect } from 'react';
+import { getCookie } from 'cookies-next';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import LikeView from '@/components/Like/LikeView';
 import axiosInstance from '@/utils/axios';
 
@@ -11,9 +15,15 @@ interface PropsType {
 export default function LikeContainer({ authorNickname, title }: PropsType) {
   const [isLiked, setIsLiked] = useState(false);
   const [likeCnt, setLikeCnt] = useState(0);
+  const accessToken = getCookie('accessToken');
 
   // 글에 대한 좋아요 여부 체크
   function checkUserLikesArticle(authorNickname: string, title: string) {
+    // 비로그인 유저
+    if (!accessToken) {
+      return;
+    }
+
     axiosInstance
       .get(`/articles/check-like/${authorNickname}/${title}`)
       .then((res) => {
@@ -29,6 +39,21 @@ export default function LikeContainer({ authorNickname, title }: PropsType) {
   }
 
   function handleLike() {
+    // 비로그인 유저는 좋아요 불가
+    if (!accessToken) {
+      toast.error('로그인 후 이용가능합니다.', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'colored',
+      });
+      return;
+    }
+
     // positive update
     setLikeCnt(isLiked ? likeCnt - 1 : likeCnt + 1);
     setIsLiked(!isLiked);
@@ -41,9 +66,19 @@ export default function LikeContainer({ authorNickname, title }: PropsType) {
       .catch((err) => {
         console.log(err);
         // positive update한 내용 원상복구
-        // TODO: 좋아요 반영에 실패했습니다 alert
         setLikeCnt(isLiked ? likeCnt - 1 : likeCnt + 1);
         setIsLiked(!isLiked);
+
+        toast.error('좋아요 반영에 실패했습니다.', {
+          position: 'top-right',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'colored',
+        });
       });
   }
 
@@ -52,5 +87,10 @@ export default function LikeContainer({ authorNickname, title }: PropsType) {
     setLikeCount(authorNickname, title);
   }, []);
 
-  return <LikeView likeCount={likeCnt} isLiked={isLiked} handleLike={handleLike} />;
+  return (
+    <>
+      <ToastContainer />
+      <LikeView likeCount={likeCnt} isLiked={isLiked} handleLike={handleLike} />
+    </>
+  );
 }
