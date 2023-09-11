8816eb18eeb626057013a8055ea34f9aac94d371
권유리 <99jiyusu@gmail.com>
2023-09-11 09:32:00 +0900

    [FIN-266] feat: 미리보기 컴포넌트에서 좋아요 버튼 클릭 방지

--- components/Icons/HeartIcon.tsx
+++ components/Icons/HeartIcon.tsx
@@ -6,12 +6,14 @@ interface PropsType {
   height?: number;
   strokeColor?: string;
   isFilled?: boolean;
+  canClick?: boolean;
 }
 export default function HeartIcon({
   width = 18,
   height = 18,
   strokeColor = '#CCCCCC',
   isFilled,
+  canClick = false,
 }: PropsType) {
   // 활성 상태에 따라 색상을 변경
   const fillColor = isFilled ? '#FA8072' : 'none'; // FA8072: 살몬
@@ -24,7 +26,7 @@ export default function HeartIcon({
       viewBox={`0 0 18 18`}
       fill={fillColor}
       xmlns="http://www.w3.org/2000/svg"
-      style={{ cursor: 'pointer' }}
+      style={{ cursor: canClick ? 'pointer' : 'default' }}
     >
       <path
         d="M9.00072 15L14.9039 9.20456L15.3559 8.69618C16.3581 7.25616 16.1752 5.32704 14.9159 4.09107C13.4763 2.67806 11.1343 2.67806 9.69438 4.09138L9.01456 4.75912L8.3024 4.05964C6.86249 2.64679 4.52077 2.64679 3.08166 4.05964C2.38399 4.74522 2 5.65522 2 6.62254C2 7.12477 2.10826 7.6292 2.31385 8.0872L2.37466 8.17532C2.55386 8.5452 2.7958 8.88096 3.09373 9.17376L3.80411 9.89851L9.00072 15Z"
--- components/Like/LikeView.tsx
+++ components/Like/LikeView.tsx
@@ -11,7 +11,13 @@ export default function LikeView({ likeCount, isLiked, handleLike }: PropsType) 
   return (
     <>
       <button type="button" onClick={handleLike}>
-        <HeartIcon width={24} height={24} strokeColor={'#999999'} isFilled={isLiked} />
+        <HeartIcon
+          width={24}
+          height={24}
+          strokeColor={'#999999'}
+          isFilled={isLiked}
+          canClick={true}
+        />
       </button>
       <span className="text-[#999999] mt-[2px]">{likeCount}</span>
     </>
