b6375fe8636f19b76437e0a7c2d424079e48598a
권유리 <99jiyusu@gmail.com>
2023-09-12 12:39:34 +0900

    [FIN-300] feat: toast 커스텀 훅 구현

---
+++ hooks/toast.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function useToast() {
+  const showErrorToast = (message: string) => {
+    console.log(message);
+    toast.error(`${message}`, {
+      position: 'top-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+    });
+  };
+
+  return [showErrorToast];
+}
