43e30252fd1fcaef6879048df902440d47d1429c
권유리 <99jiyusu@gmail.com>
2023-09-27 19:19:21 +0900

    [FIN-364] feat: 댓글 조회 기능

--- types/Article.ts
+++ types/Article.ts
@@ -1,11 +1,11 @@
 // 글 작성, 수정 시 필요한 필드
-export default interface ArticleType {
+export interface ArticleType {
   title: string;
   body: string;
   thumbnail: string;
 }
 
-export default interface ArticlePreviewType {
+export interface ArticlePreviewType {
   id: number;
   title: string;
   body: string;
